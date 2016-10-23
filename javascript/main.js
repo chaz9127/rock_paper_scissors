cb219168c76b72efab800ae49ce8ff594b810e2e
CJ <cj@gamersensei.com>
2016-10-23 04:23:16 -0400

    front end design implemented

--- javascript/main.js
+++ javascript/main.js
@@ -2,10 +2,12 @@
 var rockButton = document.getElementById('rock');
 var paperButton = document.getElementById('paper');
 var scissorsButton = document.getElementById('scissors');
-var diplayResult = document.getElementById('result');
 var computerHand = document.getElementById('computer-hand');
+var userHand = document.getElementById('user-hand');
 var winPercent = document.getElementById('win-percent');
 var totalGames = document.getElementById('total-games');
+var computerWins = document.getElementById('computer-wins');
+var userWins = document.getElementById('user-wins');
 
 rockButton.onclick = buttonClicked;
 paperButton.onclick = buttonClicked;
@@ -15,6 +17,7 @@ var gameObject = {
   "totalGames": 0,
   addGame: function() {
     this.totalGames++;
+    totalGames.innerHTML = this.totalGames;
   }
 };
 
@@ -23,6 +26,7 @@ var computerObject = {
   "type": "computer",
   win: function() {
     this.totalWins++;
+    computerWins.innerHTML = this.totalWins;
   }
 };
 
@@ -31,19 +35,24 @@ var userObject = {
   "type": "user",
   win: function() {
     this.totalWins++;
+    userWins.innerHTML = this.totalWins;
   }
 };
 
 function buttonClicked(event) {
   var computerPlay = computerHandChoice();
-  compareResults(computerPlay, this.value);
+  userHand.style.color = "#000";
+  computerHand.style.color = "#000";
+  var hand = this.getAttribute("data-hand")
+  userHand.innerHTML = hand
+  compareResults(computerPlay, hand);
 }
 
 function computerHandChoice(){
   var pick = Math.floor(Math.random()*(2-0+1)+0);
   var choices = ["rock", "paper", "scissors"];
   var choice = choices[pick];
-  computerHand.innerHTML = "<h2 id='computer-hand'>" + choice + "</h2>";
+  computerHand.innerHTML =  choice;
   return choice;
 }
 
@@ -57,12 +66,14 @@ function tie(){
 }
 
 function postResult(winner) {
-  var result = "It's a tie!"
   if(winner) {
-    result = "The <strong>" + winner.type + "</strong> wins!";
+    var winnerElement = document.getElementById(winner.type + "-hand");
+    winnerElement.style.color = "green";
+  }else {
+    userHand.style.color = "#8b0000";
+    computerHand.style.color = "#8b0000";
   }
   gameObject.addGame();
-  diplayResult.innerHTML = result;
   winPercent.innerHTML = Math.floor((computerObject.totalWins / gameObject.totalGames) * 100) + "%";
   totalGames.innerHTML = gameObject.totalGames;
 }
